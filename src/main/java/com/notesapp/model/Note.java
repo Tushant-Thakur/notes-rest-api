c6f307c2650919cf9447691b8e0cb39d77aeb8cb
Rahul-Mudgal9 <rahulmudgal886@gmail.com>
2026-07-22 12:40:43 +0530

    Revert "Added new features and updated project files"
    This reverts commit f7175923c5ad627f9f1fcec2eac518a5ea25f52c.

---
+++ src/main/java/com/notesapp/model/Note.java
@@ -0,0 +1,30 @@
+package com.notesapp.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    private String content;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+
+    public void setContent(String content) { this.content = content; }
+}
